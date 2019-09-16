d80f24bcb11f96a1b110b6b986491961b0520014
michael <mikelkarije@gmail.com>
2019-09-16 12:03:06 +0300

    addition of about and home html

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-from app import app
-app.init_app(app)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1> Hello World </h1>'
+
+@app.route('/about')
+def about():
+    return '<h1> Hello World welcome to the about page </h1>'
+
 
 if __name__ == '__main__':
     app.run(debug = True)
