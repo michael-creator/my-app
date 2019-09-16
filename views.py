bebff338b508eb78df5578192cf6b5d6e7e2c8ad
michael <mikelkarije@gmail.com>
2019-09-16 17:29:55 +0300

    this commit adds sign up form to our app

--- views.py
+++ views.py
@@ -42,5 +42,19 @@ def login():
         else:
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+    def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 if __name__ == '__main__':
     app.run(debug = True)
