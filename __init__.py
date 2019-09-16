a6989cc32b9d6718e5e16fbbb4b209353bdc3b6a
michael <mikelkarije@gmail.com>
2019-09-16 14:01:52 +0300

    addition of navbar in the app webpage

--- __init__.py
+++ __init__.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 
 
 from app import views,
+
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
