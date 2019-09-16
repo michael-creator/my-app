d1af542bbd62b9559a3020d10d985039ddd74eeb
michael <mikelkarije@gmail.com>
2019-09-16 12:31:06 +0300

    this commit adds posts into our app and more info to the home.html

--- run.py
+++ run.py
@@ -1,13 +1,36 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+post[{
+    'author': 'nill blake',
+    'title':'Example of pitches',
+    'content': 'contents',
+    'date': 'jan 17 2010'
+},
+{
+    'author': 'booterkn boika',
+    'title': 'Usefull pitches',
+    'content': 'contents',
+    'date': 'jan 17 2010'
+  
+},
+{
+    'author': 'boot camp',
+    'title': 'short pitches',
+    'content': 'contents',
+    'date': 'jan 17 2010'
+  
+}
+]
 @app.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    return render_template("home.html",post=post)
+
 
 @app.route('/about')
 def about():
-    return '<h1> Hello World welcome to the about page </h1>'
+    return render_template("about.html",post=post)
 
 
 if __name__ == '__main__':
